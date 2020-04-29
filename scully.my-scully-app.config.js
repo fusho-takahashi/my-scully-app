7480810dc3bd0df51bba9d59f968df883b76932b
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-04-29 11:02:24 +0900

    ng generate @scullyio/init:blog

--- scully.my-scully-app.config.js
+++ scully.my-scully-app.config.js
@@ -3,5 +3,11 @@ exports.config = {
   projectName: "my-scully-app",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
